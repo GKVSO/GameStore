af44f3e03b361c711ee5dadf5d82213d3479f485
GKVSO <itsergey1001@gmail.com>
2025-10-18 23:58:56 +0500

    feat: enqueue styles, scripts and google fonts

--- themes/game-store/functions.php
+++ themes/game-store/functions.php
@@ -15,12 +15,33 @@
  *
  * @return void
  */
-function game_store_styles() {
+function game_store_enqueue() {
 	wp_enqueue_style(
 		'game-store-style',
 		get_stylesheet_uri(),
 		[],
 		wp_get_theme()->get( 'Version' )
 	);
+
+	wp_enqueue_style(
+		'game-store-main-style',
+		get_stylesheet_directory_uri() . '/assets/styles/gamestore.css',
+		[],
+		wp_get_theme()->get( 'Version' )
+	);
+
+	wp_enqueue_style(
+		'game-store-google-fonts',
+		'https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;700&display=swap',
+		[],
+		wp_get_theme()->get( 'Version' )
+	);
+
+	wp_enqueue_script(
+		'game-store-main-script',
+			get_stylesheet_directory_uri() . '/assets/js/gamestore.js',
+		[],
+		wp_get_theme()->get( 'Version' )
+	);
 }
-add_action( 'wp_enqueue_scripts', 'game_store_styles' );
+add_action( 'wp_enqueue_scripts', 'game_store_enqueue' );
